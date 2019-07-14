f5592716c0d40f9e77bf0735a8e81b7e20bb7231
Alexandru Stefan Andries <20018146@studenti.uniupo.it>
2019-07-14 14:10:14 +0200

    Part 2 done. added entity and game_map managers

--- engine.py
+++ engine.py
@@ -1,16 +1,29 @@
 import tcod as libtcod
 
+from render_functions import render_all, clear_all
+from entity import Entity
 from input_handlers import handle_keys
+from map_objects.game_map import GameMap
 
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    colors = {
+        'dark_wall': libtcod.Color(0,0,100),
+        'dark_ground': libtcod.Color(50,50,150)
+    }
     
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
+    player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
+    npc = Entity(int(screen_width/2 -5), int(screen_height/2-5), '@', libtcod.yellow)
+    entities = [npc, player]
 
     con = libtcod.console_new(screen_width,screen_height)
+
+    game_map = GameMap(map_width, map_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -22,13 +35,11 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
-        
+        clear_all(con, entities)
+
         action = handle_keys(key)
 
         move = action.get('move')
@@ -37,8 +48,8 @@ def main():
 
         if move : 
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
         
         if exit:
             return True
